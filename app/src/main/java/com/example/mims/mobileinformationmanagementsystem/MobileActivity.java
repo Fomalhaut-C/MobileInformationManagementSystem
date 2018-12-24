3663f9e53d54af7a5828d5317a406a6fa260a6e5
Fomalhaut-C <983608833@qq.com>
2018-12-24 17:25:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mims/mobileinformationmanagementsystem/MobileActivity.java
@@ -0,0 +1,13 @@
+package com.example.mims.mobileinformationmanagementsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MobileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile);
+    }
+}
