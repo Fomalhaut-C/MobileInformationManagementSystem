da92716cd5d28a989a521c54ed6e81741773ba2a
Yimio <643549780@qq.com>
2018-12-27 09:31:22 +0800

    Login Register Complete

--- app/src/main/java/com/example/mims/mobileinformationmanagementsystem/Register/RegisterActivity.java
+++ app/src/main/java/com/example/mims/mobileinformationmanagementsystem/Register/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,33 +14,29 @@ import com.example.mims.mobileinformationmanagementsystem.R;
 
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText et_phoneNum,et_user,et_mail,et_password;
+    private EditText et_phoneNum,et_user,et_mail,et_password,et_rePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         initView();
-//        Long phoneNum = Long.valueOf(et_phoneNum.getText().toString().trim());
-//        String user = et_user.getText().toString().trim();
-//        String mail = et_mail.getText().toString().trim();
-//        System.out.println(et_user.getText().toString());
-//        System.out.println(mail);
-//        final String password = et_password.getText().toString().trim();
-//          final String rePassword = et_rePassword.getText().toString().trim();
+
         findViewById(R.id.btn_register_register).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*if (user.equals("") || mail.equals("") || password.equals("") || rePassword.equals("")){
+                String user = et_user.getText().toString().trim();
+                String mail = et_mail.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+                String rePassword = et_rePassword.getText().toString().trim();
+                if (TextUtils.isEmpty(et_phoneNum.getText()) || user.equals("") || mail.equals("") || password.equals("") || rePassword.equals("")){
                     Toast.makeText(getApplicationContext(),"文本框不可以留空，请重新检查,认真填写！",Toast.LENGTH_SHORT).show();
                 }else if (!rePassword.equals(password)){
                     Toast.makeText(getApplicationContext(),"两次输入的密码不一致，请重新输入！",Toast.LENGTH_SHORT).show();
                 }else {
                     Register();
                     Toast.makeText(getApplicationContext(),"注册成功",Toast.LENGTH_SHORT).show();
-                }*/
-                Register();
-                Toast.makeText(getApplicationContext(),"注册成功",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -61,6 +58,6 @@ public class RegisterActivity extends AppCompatActivity {
         et_user = findViewById(R.id.et_register_user);
         et_mail = findViewById(R.id.et_register_mail);
         et_password = findViewById(R.id.et_register_password);
-//        et_rePassword = findViewById(R.id.et_register_rePassword);
+        et_rePassword = findViewById(R.id.et_register_rePassword);
     }
 }
