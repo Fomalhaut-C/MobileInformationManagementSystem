702cda444415561a0070d2451df5c46932eca387
Fomalhaut-C <983608833@qq.com>
2018-12-27 16:29:05 +0800

    Login Register Complete

--- app/src/main/java/com/example/mims/mobileinformationmanagementsystem/Login/LoginActivity.java
+++ app/src/main/java/com/example/mims/mobileinformationmanagementsystem/Login/LoginActivity.java
@@ -5,7 +5,9 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.mims.mobileinformationmanagementsystem.MobileActivity;
@@ -16,7 +18,8 @@ import com.example.mims.mobileinformationmanagementsystem.Register.RegisterActiv
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText et_account,et_password;
-
+    private CheckBox login_remember;
+    private SharedPreferences remember_sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +28,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         et_password = findViewById(R.id.et_login_password);
         findViewById(R.id.tv_login_register).setOnClickListener(this);
         findViewById(R.id.btn_login_login).setOnClickListener(this);
-        findViewById(R.id.rbtn_login_remember_pass).setOnClickListener(this);
+        login_remember = findViewById(R.id.cbox_login_remember_password);
+        login_remember.setOnClickListener(this);
+        remember_sp = getSharedPreferences("RememberInfo",MODE_PRIVATE);
+        Boolean isRemember = remember_sp.getBoolean("checkbox",false);
+        if (isRemember){
+            Long phoneNum = remember_sp.getLong("phoneNum",1);
+            String user = remember_sp.getString("user","");
+            String mail = remember_sp.getString("mail","");
+            String password = remember_sp.getString("password","");
+            if (phoneNum != 1){
+                et_account.setText(String.valueOf(phoneNum));
+            }else if (!user.equals("")){
+                et_account.setText(user);
+            }else if (!mail.equals("")){
+                et_account.setText(mail);
+            }
+            et_password.setText(password);
+            login_remember.setChecked(true);
+        }else {
+            login_remember.setChecked(false);
+        }
     }
 
     @Override
@@ -37,32 +60,52 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 finish();
                 break;
             case R.id.btn_login_login:
+                if (login_remember.isChecked()){
+                    Remember();
+                }else {
+                    SharedPreferences.Editor editor = getSharedPreferences("RememberInfo", MODE_PRIVATE).edit();
+                    editor.clear();
+                    editor.apply();
+                }
                 Login();
-                break;
-            case R.id.rbtn_login_remember_pass:
-                /*String account = sharedPreferences.getString("account","");
-                String password = sharedPreferences.getString("password","");
-                et_account.setText(account);
-                et_password.setText(password);*/
                 break;
         }
     }
 
     public void Login(){
-        SharedPreferences register_sp;
-        register_sp = getSharedPreferences("AccountInfo",MODE_PRIVATE);
+        SharedPreferences register_sp = getSharedPreferences("AccountInfo",MODE_PRIVATE);
         Long phoneNum = register_sp.getLong("phoneNum",1);
         String user = register_sp.getString("user","");
         String mail = register_sp.getString("mail","");
-        String password = register_sp.getString("password","");
+        String register_password = register_sp.getString("password","");
         String account = et_account.getText().toString().trim();
-        String password1 = et_password.getText().toString().trim();
-        if (account.equals(String.valueOf(phoneNum)) || account.equals(user) || account.equals(mail) && password1.equals(password)){
+        String password = et_password.getText().toString().trim();
+        if (account.equals(String.valueOf(phoneNum)) || account.equals(user) || account.equals(mail) && password.equals(register_password)){
             Intent intent = new Intent(getApplicationContext(),MobileActivity.class);
             startActivity(intent);
             finish();
         }else {
             Toast.makeText(getApplicationContext(),"帐号或者密码错误，请重新输入！",Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void Remember(){
+        SharedPreferences.Editor editor = getSharedPreferences("RememberInfo", MODE_PRIVATE).edit();
+        SharedPreferences register_sp = getSharedPreferences("AccountInfo",MODE_PRIVATE);
+        remember_sp = getSharedPreferences("RememberInfo",MODE_PRIVATE);
+            Long phoneNum = register_sp.getLong("phoneNum",1);
+            String user = register_sp.getString("user","");
+            String mail = register_sp.getString("mail","");
+            String account = et_account.getText().toString().trim();
+            if (account.equals(String.valueOf(phoneNum))){
+                editor.putLong("phoneNum", Long.parseLong(account));
+            }else if(account.equals(user)){
+                editor.putString("user", user);
+            }else if (account.equals(mail)){
+                editor.putString("mail", mail);
+            }
+            editor.putString("password", et_password.getText().toString());
+            editor.putBoolean("checkbox",true);
+            editor.apply();
     }
 }
